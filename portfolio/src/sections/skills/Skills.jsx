4a5cd7fb1689a4552594df48c8291a19daf5cfda
Greque <ichijou_raku@hotmail.com>
2025-02-26 21:29:13 -0300

    a

--- portfolio/src/sections/skills/Skills.jsx
+++ portfolio/src/sections/skills/Skills.jsx
@@ -18,13 +18,13 @@ function Skills({theme="sTheme", myId="idSkills"}){
                                     <td>HTML, CSS, JS</td>
                                 </tr>
                                 <tr>
+                                    <td>PHP</td>
+                                </tr>
+                                <tr>
                                     <td>React.JS, Bootstrap</td>
                                 </tr>
                                 <tr>
                                     <td>MySQL</td>
-                                </tr>
-                                <tr>
-                                    <td>PHP</td>
                                 </tr>
                                 <tr>
                                     <td>Python</td>
